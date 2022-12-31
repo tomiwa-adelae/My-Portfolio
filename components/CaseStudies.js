38f6235336d5d621c22397b92af5f857097e869b
thetommedia@gmail.com <thetommedia@gmail.com>
2022-12-31 20:09:35 +0100

    Yes added new projects

--- components/CaseStudies.js
+++ components/CaseStudies.js
@@ -10,13 +10,59 @@ const CaseStudies = () => {
          </div>
 
          <div className="content">
-            <div className="file-case case">
+            {/* <div className="file-case case">
                <img src="files.jpg" alt="Files App" />
                <Link href="files">
                   <div className="case-preview">
                      <span>
                         <h3>Files App</h3>
                         <p>あらゆる種類のファイルを追跡します</p>
+                        <button className="btn btn-primary">
+                           Case Study <i className="fas fa-angle-right"></i>
+                        </button>
+                     </span>
+                  </div>
+               </Link>
+            </div> */}
+            <div className="shop-case case">
+               <img src="shop.jpg" alt="Shop App" />
+               <Link href="shop">
+                  <div className="case-preview">
+                     <span>
+                        <h3>S-SHOP</h3>
+                        <p>
+                           An app for buying and selling in school environment
+                        </p>
+                        <button className="btn btn-white">
+                           Case Study <i className="fas fa-angle-right"></i>
+                        </button>
+                     </span>
+                  </div>
+               </Link>
+            </div>
+
+            <div className="wings-case case">
+               <img src="wings.jpg" alt="Prophetic wings" />
+               <Link href="wings">
+                  <div className="case-preview">
+                     <span>
+                        <h3>Prophetic Wings</h3>
+                        <p>A website for daily loads of the word of God</p>
+                        <button className="btn btn-green">
+                           Case Study <i className="fas fa-angle-right"></i>
+                        </button>
+                     </span>
+                  </div>
+               </Link>
+            </div>
+
+            <div className="day-case case">
+               <img src="day.jpg" alt="D-Days" />
+               <Link href="days">
+                  <div className="case-preview">
+                     <span>
+                        <h3>D-Days</h3>
+                        <p>An app for keeping track of birthdays</p>
                         <button className="btn btn-primary">
                            Case Study <i className="fas fa-angle-right"></i>
                         </button>
@@ -33,21 +79,6 @@ const CaseStudies = () => {
                         <h3>Pick Me Fashions</h3>
                         <p>A shopping website</p>
                         <button className="btn btn-dark">
-                           Case Study <i className="fas fa-angle-right"></i>
-                        </button>
-                     </span>
-                  </div>
-               </Link>
-            </div>
-
-            <div className="day-case case">
-               <img src="day.jpg" alt="D-Days" />
-               <Link href="days">
-                  <div className="case-preview">
-                     <span>
-                        <h3>D-Days</h3>
-                        <p>An app for keeping track of birthdays</p>
-                        <button className="btn btn-primary">
                            Case Study <i className="fas fa-angle-right"></i>
                         </button>
                      </span>
---
+++ pages/shop.js
@@ -0,0 +1,154 @@
+import Meta from '../components/Meta';
+import Link from 'next/link';
+
+const shop = () => {
+   return (
+      <div className="shop">
+         <Meta
+            title="Tomiwa Adelae | S-SHOP"
+            description="We buy and sell"
+            keywords="shops, S-SHOP app, E-ECOMMERCE app, app"
+         />
+         <header>
+            <Link href="/">
+               <h3>
+                  Tomiwa <span className="text-shop-grey">Adelae</span>
+               </h3>
+            </Link>
+            <Link href="/contact">
+               <div className="btn btn-shop-grey">Get In Touch</div>
+            </Link>
+         </header>
+         <div className="case-showcase">
+            <img src="shop.jpg" alt="" />
+            <div className="details">
+               <div className="intro">
+                  <h1>
+                     S-<span>SHOP</span>
+                  </h1>
+                  <h5>An app for buying and selling in school environment</h5>
+               </div>
+               <div
+                  onClick={() => {
+                     window.scroll({
+                        top: 420,
+                        behavior: 'smooth',
+                     });
+                  }}
+                  className="icon-shop-grey"
+               >
+                  <i className="fas fa-angle-down"></i>
+               </div>
+               <div className="bottom-details">
+                  <h6>
+                     <span className="text-shop-grey">Role</span> Full Stack
+                     Developer
+                  </h6>
+                  <h6>
+                     <span className="text-shop-grey">Period</span> Late 2022
+                  </h6>
+               </div>
+            </div>
+         </div>
+         <div className="about-project">
+            <div className="content">
+               <h3>The Project</h3>
+               <p>
+                  Files App is a meticulously designed web application for
+                  storing any type of information, photos, or contacts. Files
+                  App is intended to meet the majority of file structure and
+                  information requirements.
+               </p>
+               <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://tomiwaadelae.netlify.app/"
+               >
+                  <div className="btn btn-shop-grey">
+                     Visit Website <i className="fas fa-angle-right"></i>
+                  </div>
+               </a>
+            </div>
+         </div>
+         <div className="project-design-shop-grey">
+            <div className="content">
+               <h3>Design</h3>
+
+               <div className="boxes">
+                  <div className="box one">
+                     <img src="shopone.jpg" alt="" />
+                  </div>
+                  <div className="box two">
+                     <img src="shoptwo.jpg" alt="" />
+                  </div>
+                  <div className="box one">
+                     <img src="shopthree.jpg" alt="" />
+                  </div>
+               </div>
+            </div>
+         </div>
+
+         <footer>
+            <h2
+               onClick={() => {
+                  window.scroll({
+                     top: 0,
+                     behavior: 'smooth',
+                  });
+               }}
+            >
+               Tomiwa <span className="text-shop-grey">Adelae</span>
+            </h2>
+
+            <div className="icons">
+               <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.facebook.com/adelae.tomiwa/"
+               >
+                  <i className="fab fa-facebook"></i>
+               </a>
+               <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.instagram.com/thetommedia/"
+               >
+                  <i className="fab fa-instagram"></i>
+               </a>
+               <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://twitter.com/TheTomMedia"
+               >
+                  <i className="fab fa-twitter"></i>
+               </a>
+               <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.linkedin.com/in/tomiwa-ademola-6a08181b7/"
+               >
+                  <i className="fab fa-linkedin"></i>
+               </a>
+               <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://github.com/AdemolaTomiwa"
+               >
+                  <i className="fab fa-github"></i>
+               </a>
+               <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="mailto:adelaetomiwa6@gmail.com"
+               >
+                  <i className="fas fa-envelope"></i>
+               </a>
+            </div>
+
+            <p>&copy; Tomiwa Adelae 2022</p>
+         </footer>
+      </div>
+   );
+};
+
+export default shop;
