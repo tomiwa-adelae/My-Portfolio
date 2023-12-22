0f79d3cca4cac8136b4ffe4937c5c10e9538669f
Tomiwa Adelae <thetommedia@gmail.com>
2023-12-22 21:53:42 +0100

    Made some changes

--- components/Meta.js
+++ components/Meta.js
@@ -1,24 +1,27 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 const Meta = ({ title, keywords, description }) => {
-   return (
-      <Head>
-         <title>{title}</title>
-         <meta name="viewport" content="width=device-width, initial-scale=1" />
-         <meta name="keywords" content={keywords} />
-         <meta name="description" content={description} />
-         <meta charSet="utf-8" />
-         <link rel="icon" href="/favicon.ico" />
-      </Head>
-   );
+	return (
+		<Head>
+			<title>{title}</title>
+			<meta
+				name="viewport"
+				content="width=device-width, initial-scale=1"
+			/>
+			<meta name="keywords" content={keywords} />
+			<meta name="description" content={description} />
+			<meta charSet="utf-8" />
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+	);
 };
 
 Meta.defaultProps = {
-   title: 'Tomiwa Adelae',
-   keywords:
-      'Tomiwa, Adelae, Tomiwa Adelae, Portfolio, website, website developer, web, website development, programmer, programming, backend, frontend, full stack, stack',
-   description:
-      'Tomiwa Adelae Portfolio, Greatest website developer in the world',
+	title: "Tomiwa Adelae",
+	keywords:
+		"Tomiwa, Adelae, Tomiwa Adelae, Adetomiwa, Adetomiwa Adelae, Portfolio, website, website developer, web, website development, programmer, programming, backend, frontend, full stack, stack",
+	description:
+		"Tomiwa Adelae Portfolio, Greatest website developer in the world",
 };
 
 export default Meta;
