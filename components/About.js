d23736ff2b1e4c85893a398ce186d8fec2f892e5
Tomiwa Adelae <thetommedia@gmail.com>
2024-03-14 19:11:09 +0100

    Made some changes

--- components/About.js
+++ components/About.js
@@ -5,11 +5,12 @@ const About = () => {
 				<h3>Hi, I am Tomiwa Adelae. Nice to meet you</h3>
 				<p>
 					As a website programmer and freelancer for over four years,
-					I've completed personal projects, remote work for agencies,
-					and consulted for startups. Collaborating with talented
-					individuals, I've created digital products for diverse uses.
-					I'm quietly confident, naturally curious, and continually
-					strive to enhance my skills with each design challenge.
+					I have completed personal projects, remote work for
+					agencies, and consulted for startups. Collaborating with
+					talented individuals, I have created digital products for
+					diverse uses. I am quietly confident, naturally curious, and
+					continually strive to enhance my skills with each design
+					challenge.
 				</p>
 			</div>
 		</div>
